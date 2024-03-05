6e44393d42c4490e61041d067d4e8a361503c69d
piyush.patel <piyush.patel@rws.com>
2024-03-05 11:47:36 +0530

    Get to Post

--- index.js
+++ index.js
@@ -1,18 +1,27 @@
 const express = require('express');
-const bodyparser = require('body-parser');
+const bodyParser = require('body-parser');
 const cors = require('cors');
 const { translate } = require('@vitalets/google-translate-api');
 
 const app = express();
 
 app.use(cors());
-app.use(bodyparser.json());
+app.use(bodyParser.json());
 
 app.listen(3000, () => {
     console.log('Server running');
-})
+});
 
-app.get("/translate", async  (req, res) => {
-    const { text } = await translate(req.body.query, { to: req.body.language });
-    res.send(text)
-})
+app.post("/translate", async (req, res) => {
+    try {
+        const { query, language } = req.body;
+        if (!query || !language) {
+            return res.status(400).json({ error: 'Text and language are required.' });
+        }
+        const translation = await translate(query, { to: language });
+        res.send(translation.text)
+    } catch (error) {
+        console.error('Translation error:', error);
+        res.status(500).json({ error: 'An error occurred during translation.' });
+    }
+});
